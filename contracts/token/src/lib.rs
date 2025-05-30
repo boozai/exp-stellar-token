9219b42ddb29f6b63dbe57b34705c337b58b710c
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-05-30 18:10:22 +1000

    actually store balances

--- contracts/token/src/lib.rs
+++ contracts/token/src/lib.rs
@@ -6,15 +6,24 @@ pub struct Contract;
 
 #[contractimpl]
 impl Contract {
-    pub fn mint(env: Env, to: Address, amount: i128) {
-        env.events().publish((symbol_short!("mint"), &to), amount);
-    }
-
     pub fn symbol(env: Env) -> String {
         String::from_str(&env, "ABC")
     }
 
+    pub fn mint(env: Env, to: Address, amount: i128) {
+        let mut to_bal: i128 = env.storage().persistent().get(&to).unwrap_or_default();
+        to_bal += amount;
+        env.storage().persistent().set(&to, &to_bal);
+        env.events().publish((symbol_short!("mint"), &to), amount);
+    }
+
     pub fn transfer(env: Env, from: Address, to: Address, amount: i128) {
+        let mut from_bal: i128 = env.storage().persistent().get(&from).unwrap_or_default();
+        let mut to_bal: i128 = env.storage().persistent().get(&to).unwrap_or_default();
+        from_bal -= amount;
+        to_bal += amount;
+        env.storage().persistent().set(&from, &from_bal);
+        env.storage().persistent().set(&to, &to_bal);
         env.events().publish((symbol_short!("transfer"), &from, &to), amount);
     }
 }
